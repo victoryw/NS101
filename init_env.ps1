246ca59bf3a895bdf0741fea9fb4272d0914ce2c
Your Name <you@example.com>
2016-07-20 00:26:30 +0800

    i do not want to say any

--- init_env.ps1
+++ init_env.ps1
@@ -1,7 +1,15 @@
 param(
-    $NScaffoldCodeBasePath
+    [string]$NScaffoldCodeBasePath,
+    $WithChocolatey=$FALSE
 )
 
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+if($NScaffoldCodeBasePath){
+    if($WithChocolatey){
+        iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+   git clone https://github.com/xiaoyvr/NScaffold.git $NScaffoldCodeBasePath 
+}
+else {
+    Write-host "please input the NScaffold code base full path by the param -NScaffoldCodeBasePath"
+}
 
-git clone https://github.com/xiaoyvr/NScaffold.git $NScaffoldCodeBasePath
